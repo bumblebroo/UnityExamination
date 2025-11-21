aaeee57f7b13a03f856443f34e1f938c65873e50
bumblebroo <otis.gustafsson@hotmail.com>
2025-11-21 10:42:49 +0100

    Kriterie : Start meny

---
+++ Assets/Scripts/ApplicationHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This component can be used to access methods for changing the scene, and quitting the game
+// It can be used together with UnityEventOnTrigger, or UI-button-events, to decide when a scene should be changed or the game should be closed
+public class ApplicationHandler : MonoBehaviour
+{
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadSceneAsync(index, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
